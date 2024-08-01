66c1f35e51868b9dabf3c923a1c56accac220459
yoxcu <mick@yoxcu.de>
2024-08-01 07:46:53 +0200

    cleaned up simulation

--- simulate.py
+++ simulate.py
@@ -1,33 +1,28 @@
-from theory import qwp, hwp, printMatrix
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.optimize import brute
 
-import matplotlib.pyplot as plt
-
-printMatrix(qwp(0))
-printMatrix(qwp(180))
-#for i in np.arange(0, 180, 22.5):
-#    printMatrix(qwp(i) @ qwp(i + 90))
-#    print(qwp(i))
+from PolarizationCompensation.Theory import qwp, hwp, printMatrix
 
 
-def get_angles(matrix, x0=[0, 0, 0], output=False, first=False):
+# Change this to waveplate setup
+def model(alpha, beta, gamma):
+    return qwp(gamma) @ hwp(beta) @ qwp(alpha)
 
-    def model(alpha, beta, gamma):
-        return qwp(gamma) @ hwp(beta) @ qwp(alpha)
+
+def get_angles(matrix, x0=[0, 0, 0], output=False, r=[8, 8, 8], Ns=10):
 
     def f(val):
         alpha, beta, gamma = val
         A = model(alpha, beta, gamma) - matrix
         return abs(A).sum()
 
+    r = np.array(r)
+    x0 = np.array(x0)
+
     a, b, g = x0
-    r = 8
-    if first:
-        r = 180
-    res = brute(f, ((a, a + r), (b, b + r), (g, g + r)),
-                Ns=100,
-                full_output=True)
+    ar, br, gr, = x0 + r
+    res = brute(f, ((a, ar), (b, br), (g, gr)), Ns=Ns, full_output=True)
     alpha, beta, gamma = [x % 360 for x in res[0]]
     if output:
         print(
@@ -49,26 +44,30 @@ positions = [[0, 0, 90]]
 xs = []
 es = []
 first = True
-for i in np.arange(0, 181, 180):
-    print(i)
-    print(positions[-1])
-    a, b, g, e = get_angles(
-        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
-        output=True,
-        x0=positions[-1],
-        first=first)
-    positions.append([a, b, g])
+for i in np.arange(0, 181, 10):
+    r = [8, 8, 8]
+    if first:
+        r = [0, 0, 0]
+        first = False
+    a, b, g, e = get_angles(hwp(i), output=False, x0=positions[-1], r=r)
+    positions.append([i, b, i + 90])
     xs.append(i)
     es.append(e)
-    first = False
+    if e > 2:
+        print("Error too high")
+        break
 
 positions = np.array(positions[1:])
-print(positions)
 pt = np.transpose(positions)
 
-print(pt)
+a, b, g, e = get_angles(qwp(0), output=False, x0=[0, 0, 0], r=[180, 180, 180])
+print("Values for fixed QWP at 0deg: alpha={:.2f}, beta={:.2f}, gamma={:.2f}".
+      format(a, b, g))
+
 for i, ys in enumerate(pt):
     plt.plot(xs, ys, label=i)
 plt.plot(xs, np.array(es) * 10**7, label="error *10^7")
+plt.xlabel("Rotation angle of simulated HWP in deg")
+plt.ylabel("Rotation angle of actual WPs in deg")
 plt.legend()
 plt.show()
